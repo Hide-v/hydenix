f5ea60e459f3faf54ce25e135ab15105b9a9d07c
Yun Zhou <Vcmingri19@outlook.com>
2025-11-10 22:14:20 +0800

    Update default.nix

--- template/modules/hm/default.nix
+++ template/modules/hm/default.nix
@@ -1,14 +1,18 @@
-{ ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports = [
     # ./example.nix - add your modules here
+    ./hyprland.nix
   ];
 
   # home-manager options go here
   home.packages = with pkgs; [
     # pkgs.vscode - hydenix's vscode version
     # pkgs.userPkgs.vscode - your personal nixpkgs version
+    google-chrome
+    lazygit
+    localsend
   ];
 
   # hydenix home-manager options go here
