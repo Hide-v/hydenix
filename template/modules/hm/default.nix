cd704723d08d5598668904e4c5903f8b12d605d3
Yun Zhou <Vcmingri19@outlook.com>
2025-11-10 17:36:52 +0800

    Update default.nix

--- template/modules/hm/default.nix
+++ template/modules/hm/default.nix
@@ -6,7 +6,7 @@
   ];
 
   # home-manager options go here
-  home.packages = [
+  home.packages = with pkgs; [
     # pkgs.vscode - hydenix's vscode version
     # pkgs.userPkgs.vscode - your personal nixpkgs version
   ];
